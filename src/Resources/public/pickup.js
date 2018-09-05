26cd120a6e9a3d885eeafb94488922e285b857be
magentix <mvion@magentix.fr>
2018-09-05 16:19:57 +0200

    OSM Map added

--- src/Resources/public/pickup.js
+++ src/Resources/public/pickup.js
@@ -56,7 +56,7 @@ var pickupClass = function (options) {
      * @param {string} method
      * @param {Object} params
      */
-    this.list = function(item, index, method, params) {
+    this.list = function (item, index, method, params) {
         var pickup = this;
         params.index = index;
 
@@ -85,7 +85,7 @@ var pickupClass = function (options) {
      *
      * @param {int} status
      */
-    this.loading = function(status) {
+    this.loading = function (status) {
         var form = $('form[name="sylius_checkout_select_shipping"]');
 
         form.removeClass('loading');
@@ -124,6 +124,92 @@ var pickupClass = function (options) {
      */
     var getUrl = function(method) {
         return vars.url + '/' + method;
+    };
+
+    this.construct(options);
+};
+
+/**
+ * Map OSM Class Class
+ *
+ * @param {Object} options
+ */
+var mapOsmClass = function (options) {
+    var vars = {
+        'map': null,
+        'markers': []
+    };
+
+    /**
+     * Load map
+     *
+     * @param {Object} options
+     */
+    this.construct = function (options) {
+        vars.map = L.map(options.mapId);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 18,
+            id: 'mapbox.streets'
+        }).addTo(vars.map);
+        vars.map.attributionControl.setPrefix('');
+    };
+
+    /**
+     * Add locations to map
+     *
+     * @param {Object.<number, Object>} locations - All points on the map
+     */
+    this.locations = function (locations) {
+        var osmMap = this;
+
+        if (vars.map) {
+            var i;
+            var bounds = [];
+            for (i = 0; i < locations.length; i++) {
+                if (typeof locations[i] !== 'undefined') {
+                    var latLng = [locations[i][1], locations[i][2]];
+                    var marker = L.marker(latLng, {id:locations[i][3]}).addTo(vars.map);
+                    marker.on('click', function (e) {
+                        osmMap.select(e.sourceTarget.options.id)
+                    });
+                    marker.bindPopup(locations[i][0]);
+                    vars.markers[locations[i][3]] = marker;
+
+                    bounds.push(latLng);
+                }
+            }
+
+            if (bounds.length) {
+                vars.map.fitBounds(bounds);
+            }
+
+            if (bounds.length === 1) {
+                vars.map.setZoom(15);
+            }
+        }
+    };
+
+    /**
+     * Select location in the list
+     *
+     * @param {string} inputId - Id of input element
+     */
+    this.select = function (inputId) {
+        var input = $('#' + inputId);
+        if (input) {
+            input.prop('checked', true);
+        }
+    };
+
+    /**
+     * Show marker on map
+     *
+     * @param {string} locationId - Id of the marker
+     */
+    this.update = function (locationId) {
+        if (vars.markers[locationId] && vars.map) {
+            vars.markers[locationId].openPopup();
+        }
     };
 
     this.construct(options);
