eb7d50c66aaeb065260d079172a018eadfe8be1e
magentix <mvion@magentix.fr>
2018-09-21 14:18:18 +0200

    Fix billing address render on checkout summary

--- src/EventListener/OrderInitializeCompleteListener.php
+++ src/EventListener/OrderInitializeCompleteListener.php
@@ -29,12 +29,14 @@ final class OrderInitializeCompleteListener extends PickupListener
         $pickup = $this->getPickupAddress($order);
 
         if (!empty($pickup)) {
-            $shipping = $order->getShippingAddress();
+            $shipping = clone $order->getShippingAddress();
             $shipping->setCompany($pickup['company']);
             $shipping->setStreet($pickup['street_1']);
             $shipping->setCity($pickup['city']);
             $shipping->setPostcode($pickup['postcode']);
             $shipping->setCountryCode($pickup['country']);
+
+            $order->setShippingAddress($shipping);
         }
     }
 }
